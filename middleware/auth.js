b13da9f0ef51bd1993a86cc204e8837967a0ba84
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-04-10 23:51:24 +0700

    a

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,15 +9,16 @@ async function generateTokenAndRedirect(req, res) {
             const tokenPayload = {
                 email: req.user.emails[0].value,
                 role: userData[0].Role
-            };
-            const token = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
-           
+            };
+            const token = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
+
+
             res.cookie('token', token);
 
-         
-            res.redirect('http://localhost:5173/');
-        } else {
+
+            res.redirect('http://localhost:5173/home');
+        } else {    
             return res.status(404).json({ message: 'User not found' });
         }
     } else {
@@ -29,21 +30,18 @@ function authenticateTokenForRole(role) {
     return function (req, res, next) {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1].slice(0, -1);
-        console.log("token= " + token)
         if (!token) {
-            console.log("unauthen")
             return res.sendStatus(401);
         }
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
             if (err) {
-
-
-                return res.sendStatus(403);
+                return res.redirect(403, 'http://localhost:5173/');
             }
             if (user.role !== role) {
                 return res.sendStatus(403).json({ message: 'role' });
             }
+            console.log("role: "+user.role)
             req.user = user;
             next();
         });
