b2f9e707dafd35d79c7816d6f225048f7f8562d3
TungCT123 <harrybother33@gmail.com>
2024-04-11 17:36:01 +0700

    test hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import HomePage from './pages/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './component/Header';
 import Footer from './component/Footer';
+import Signin from './pages/Signin';
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
-          <HomePage />
+          
+          <Routes>
+          <Route path="/login" element={<Signin />} />
+          <Route path="/home" element={<HomePage />} />
+          </Routes>
         </main>
         <Footer />
       </div>
