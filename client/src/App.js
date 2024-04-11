f1e11e6b3c5156f2885eede405e948f98a307f09
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-04-11 20:18:24 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
 import HomePage from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './component/Header';
-import Footer from './component/Footer';
+
 import Signin from './pages/Signin';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          
-          <Routes>
-          <Route path="/login" element={<Signin />} />
-          <Route path="/home" element={<HomePage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+
+
+
+
+      <Routes>
+        <Route path="/login" element={<Signin />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+
     </Router>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import UseRole from "../hooks/UserRole";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
 function Home() {
   const { data, user } = UseRole("http://localhost:5000/Student/getAll");
 
   return (
-    <div>
-      {data.map((student) => (
-        <div key={student.MaSV}>
-          <h2>{student.St_Fullname}</h2>
-        </div>
-      ))}
-      {user ? (
-        <div>
-          <p>Welcome</p>
-          <p>Your role: {user.role}</p>
-        </div>
-      ) : (
-        <p>Please log in</p>
-      )}
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        {data.map((student) => (
+          <div key={student.MaSV}>
+            <h2>{student.St_Fullname}</h2>
+          </div>
+        ))}
+        {user ? (
+          <div>
+            <p>Welcome</p>
+            <p>Your role: {user.role}</p>
+          </div>
+        ) : (
+          <p>Please log in</p>
+        )}
+      </main>
+      <Footer />
     </div>
   );
 }
