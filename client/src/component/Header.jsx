20b6886d79910b5aa5edda59da97b0729f0d1136
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-04-13 15:21:29 +0700

    commit

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,53 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+
 function Header() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isMenuOpen1, setIsMenuOpen1] = useState(false);
-  const menuRef = useRef(null);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-  const toggleMenu1 = () => {
-    setIsMenuOpen1(!isMenuOpen1);
+  const handleClose = () => {
+    setAnchorEl(null);
   };
-
-  const handleClickOutside = (event) => {
-    if (
-      menuRef.current &&
-      !menuRef.current.contains(event.target) &&
-      !event.target.closest(".menu-toggle")
-    ) {
-      setIsMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleClickOutsideMobile = (event) => {
-    if (
-      menuRef.current &&
-      !menuRef.current.contains(event.target) &&
-      !event.target.closest(".menu-toggle1")
-    ) {
-      setIsMenuOpen1(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("touchstart", handleClickOutsideMobile);
-    document.addEventListener("touchend", handleClickOutsideMobile);
-
-    return () => {
-      document.removeEventListener("touchstart", handleClickOutsideMobile);
-      document.removeEventListener("touchend", handleClickOutsideMobile);
-    };
-  }, []);
   return (
     <div>
       <nav className="bg-white dark:bg-gray-800  shadow py-4 ">
@@ -118,53 +88,58 @@ function Header() {
                         alt="pic"
                       />
                     </div>
-                    <div className="hidden sm:block">
-                      <button
-                        type="button"
-                        onClick={toggleMenu}
-                        className="menu-toggle flex items-center justify-center w-full rounded-md px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500"
-                        id="options-menu"
+                    <React.Fragment>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          textAlign: "center",
+                        }}
                       >
-                        <img
-                          className="w-12 h-12 rounded-full"
-                          src="https://source.unsplash.com/grayscale-photography-of-man-wearing-crew-neck-shirt-jmURdhtm7Ng"
-                          alt="pic"
-                        />
-                      </button>
-                    </div>
-
-                    {isMenuOpen && (
-                      <div
-                        ref={menuRef}
-                        className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-gray-800 ring-1 ring-black ring-opacity-5"
+                        <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+                        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+                        <Tooltip title="Account settings">
+                          <IconButton
+                            onClick={handleClick}
+                            size="small"
+                            sx={{ ml: 2 }}
+                            aria-controls={open ? "account-menu" : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? "true" : undefined}
+                          >
+                            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                          </IconButton>
+                        </Tooltip>
+                      </Box>
+                      <Menu
+                        anchorEl={anchorEl}
+                        id="account-menu"
+                        open={open}
+                        onClose={handleClose}
+                        onClick={handleClose}
+                        transformOrigin={{
+                          horizontal: "right",
+                          vertical: "top",
+                        }}
+                        anchorOrigin={{
+                          horizontal: "right",
+                          vertical: "bottom",
+                        }}
                       >
-                        <div
-                          className="py-1"
-                          role="menu"
-                          aria-orientation="vertical"
-                          aria-labelledby="options-menu"
-                        >
-                          <a
-                            href="/"
-                            className="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                            role="menuitem"
-                          >
-                            <span className="flex flex-col">
-                              <span>Profile</span>
-                            </span>
-                          </a>
-                          <a
-                            href="/"
-                            className="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-                            role="menuitem"
-                          >
-                            <span className="flex flex-col">
-                              <span>Log Out</span>
-                            </span>
-                          </a>
-                        </div>
-                      </div>
-                    )}
+                        <MenuItem onClick={handleClose}>
+                          <Avatar /> Profile
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                          <Avatar /> My account
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={handleClose}>
+                          Add another account
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>Settings</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                      </Menu>
+                    </React.Fragment>
                   </div>
                 </div>
               </div>
@@ -175,7 +150,6 @@ function Header() {
                 <button
                   type="button"
                   className="menu-toggle1 text-gray-800 dark:text-white hover:text-gray-300 inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
-                  onClick={toggleMenu1}
                 >
                   <svg
                     width="20"
@@ -189,70 +163,66 @@ function Header() {
                   </svg>
                 </button>
               </div>
-              {isMenuOpen1 && (
-                <div
-                  ref={menuRef}
-                  className="absolute flex flex-col right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-gray-800 ring-1 ring-black ring-opacity-5"
+
+              <div className="absolute flex flex-col right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
+                <NavLink
+                  exact
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
+                      : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  }
                 >
-                  <NavLink
-                    exact
-                    to="/"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
-                        : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    }
-                  >
-                    Home
-                  </NavLink>
-                  <NavLink
-                    to="/schdule"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
-                        : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    }
-                  >
-                    Schdule
-                  </NavLink>
-                  <NavLink
-                    to="/mark_report"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
-                        : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    }
-                  >
-                    Mark Report
-                  </NavLink>
-                  <NavLink
-                    to="/attendent_report"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
-                        : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    }
-                  >
-                    Attendent Report
-                  </NavLink>
-                  <NavLink
-                    to="/profile"
-                    className={({ isActive }) =>
-                      isActive
-                        ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
-                        : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                    }
-                  >
-                    Profile
-                  </NavLink>
-                  <NavLink
-                    to="/"
-                    className="text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
-                  >
-                    Log Out
-                  </NavLink>
-                </div>
-              )}
+                  Home
+                </NavLink>
+                <NavLink
+                  to="/schdule"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
+                      : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  }
+                >
+                  Schdule
+                </NavLink>
+                <NavLink
+                  to="/mark_report"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
+                      : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  }
+                >
+                  Mark Report
+                </NavLink>
+                <NavLink
+                  to="/attendent_report"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
+                      : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  }
+                >
+                  Attendent Report
+                </NavLink>
+                <NavLink
+                  to="/profile"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-black dark:text-white dark:hover:text-white px-3 py-2 rounded-md text-md font-bold"
+                      : "text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                  }
+                >
+                  Profile
+                </NavLink>
+                <NavLink
+                  to="/"
+                  className="text-gray-500 hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-md font-medium"
+                >
+                  Log Out
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
