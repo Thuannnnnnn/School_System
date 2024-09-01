96ad5f236848c5ac9bdf1a0759443e4c6430d082
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-01 12:13:28 +0700

    A

--- server/routes/Subject.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const SubjectController =require('../controller/Subject')
-const router = express.Router()
-
-router.get('/getAll', SubjectController.getAllSubject);
-router.post('/insert', SubjectController.insertSubject);
-router.put('/update', SubjectController.UpdateSubject)
-router.get('/findSubject', SubjectController.FindCodeSubject);
-module.exports = router;
